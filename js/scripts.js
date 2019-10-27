a74713736bce7a514df7c26a21a8408469a99228
Vitalii-Hapon <vitali_gapon@gmail.com>
2019-10-27 11:40:35 +0200

    update project by Vitalii Hapon

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function($){
     // let array = ['Valera', '123', '`123`13'];
     let count = 0;
 
-    $('.header_search-btn').click(function () {
+    $('.header_icon__search-btn').click(function () {
         // let a = 1;
         // a = 2;
         // a = 50;
@@ -23,24 +23,30 @@ $(document).ready(function($){
         $('.header_input-search').toggleClass('header_active-search');
     });
 
+    $('.header_icon__main-menu-btn').click(function () {
+        $('.header_main-menu_nav').toggleClass('header_main-menu_nav_active');
+    });
+
     $('.S_services-slider').owlCarousel({
         nav: true,
         margin: 30,
         loop: true,
         items: 1.5,
         autoWidth: true,
-
-
+        smartSpeed: 750,
+        mouseDrag: false,
+        touchDrag: false,
+        navText: ["<span class='arrows'></i>","<span class='arrows'></i>"],
     });
 
     $('.S_testimonials-slider').owlCarousel({
         nav: true,
-        margin: 30,
         loop: true,
         items: 1,
-        // autoWidth: true,
-
-
+        smartSpeed: 750,
+        mouseDrag: false,
+        touchDrag: false,
+        navText: ["<span class='arrows'></i>","<span class='arrows'></i>"],
     });
 
     $('.S_works_gallery_button').click(function () {
@@ -64,8 +70,11 @@ $(document).ready(function($){
             case 'web':
                 $(imgSelector + '.web').show();
                 break;
-            default:
-                $(imgSelector + '.all').show();
+            case 'development':
+                $(imgSelector + '.development').show();
+                break;
+            default :
+                $(imgSelector + '.films').show();
         }
     })
 
